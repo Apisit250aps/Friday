4c2be0c4c6126da163301a53aed6ed9fbe0af30a
Lynnn01 <jetsadathegame@gmail.com>
2023-12-10 16:13:06 +0700

    Refactor models.py: Improve readability and add new models
    This refactor improves readability by reformatting code and adding
    missing newlines. It also introduces two new models, "Boss" and "Game",
    to support additional features in the application. The "Boss" model
    represents bosses in the game and includes fields for their name and
    pick. The "Game" model represents game instances and includes fields for
    life points and level. These changes enhance the flexibility and
    maintainability of the code. No issues are referenced in this commit.
    
    Note: This generated commit message assumes that the additional context
    provided regarding the rationale behind the changes, the benefits of the
    changes, and any necessary explanations was accurately portrayed.

--- card/models.py
+++ card/models.py
@@ -16,28 +16,57 @@ ACTIVES = (
     (9, "sort"),
     (10, "swap 1"),
     (11, "swaps 2"),
-    (12, "under")
+    (12, "under"),
 )
 
-ABILITY_TYPE = (
-    (1, "Normal"),
-    (2, "Age"),
-    (3, "Skill")
+ABILITY_TYPE = ((1, "Normal"), (2, "Age"), (3, "Skill"))
+
+LEVEL_CHOICES = (
+    (1, "Easy"),
+    (2, "Medium"),
+    (3, "Hard"),
 )
+
 
 class Abilities(models.Model):
     name = models.CharField(max_length=255)
     power = models.IntegerField()
     active = models.IntegerField(choices=ACTIVES)
-    spend = models.IntegerField(choices=(
-        (1, "1"),
-        (2, "2")
-    ))
+    spend = models.IntegerField(choices=((1, "1"), (2, "2")))
     type = models.IntegerField(choices=ABILITY_TYPE)
+
 
 class Dangerous(models.Model):
     name = models.CharField(max_length=255)
     pick = models.IntegerField()
     life_point = models.IntegerField(default=0)
- 
-  
+
+
+class Boss(models.Model):
+    name = models.CharField(max_length=255)
+    pick = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Boss"
+        verbose_name_plural = "Boss"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reversed("Boss_detail", kwargs={"pk": self.pk})
+
+
+class Game(models.Model):
+    life_point = models.IntegerField(default=20)
+    level = models.IntegerField(choices=LEVEL_CHOICES, default=1)
+
+    class Meta:
+        verbose_name = "game"
+        verbose_name_plural = "games"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reversed("game_detail", kwargs={"pk": self.pk})
